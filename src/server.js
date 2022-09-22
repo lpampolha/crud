208c728a77659e5a48a3035599054769dfa9087d
Lucas <lucas.psilva@al.infnet.edu.br>
2022-09-21 21:58:10 -0300

    Swagger funcionando

--- src/server.js
+++ src/server.js
@@ -17,6 +17,7 @@ require('./database/connection')
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended:true }))
+
 app.use(
     express.json({
         limit: "10kb",
